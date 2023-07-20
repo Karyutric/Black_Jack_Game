3d5a1e1acc9855f030f992d21671b4f7362fe45b
40343454 <rmcmullan@qub.ac.uk>
2023-07-21 00:40:27 +0100

    Card Class
    Card Class - each card will have a suit and rank.

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+# Card Class - suit and rank of each card
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
+
+    
